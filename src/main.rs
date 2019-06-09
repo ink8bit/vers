d6a34fd4107e26735797861c5c09fe966096e09b
ink8bit <ink8bit@users.noreply.github.com>
2019-06-09 14:24:01 +0300

    Change scaffolding, Add structopt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 #[macro_use]
-extern crate clap;
-use clap::App;
+extern crate structopt;
+
+#[macro_use]
+extern crate serde_derive;
+use structopt::StructOpt;
+
+mod version;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+struct Opt {
+    /// Activate debug mode
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+    /// Set speed
+    #[structopt(short = "s", long = "speed", default_value = "42")]
+    speed: f64,
+}
 
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 
-    println!("{:?}", matches);
+    println!("VERSION: {}", version::get_parsed());
 }
---
+++ src/version.rs
@@ -0,0 +1,41 @@
+#[derive(Serialize, Deserialize)]
+struct Package {
+    version: String,
+}
+
+#[allow(dead_code)]
+enum Version {
+    Major,
+    Minor,
+    Patch,
+}
+
+// Returns patch version
+#[allow(dead_code)]
+fn get_patch() -> u8 {
+    0
+}
+
+// Returns minor version
+#[allow(dead_code)]
+fn get_minor() -> u8 {
+    0
+}
+
+// Returns major version
+#[allow(dead_code)]
+fn get_major() -> u8 {
+    0
+}
+
+// Returns parsed version from package.json
+pub fn get_parsed() -> String {
+    let f = std::fs::read_to_string("package.json").expect("no such file");
+    let package: Package = serde_json::from_str(&f).unwrap();
+
+    package.version
+}
+
+// Updates version in package.json by a given one
+#[allow(dead_code)]
+fn update(version: Version) {}
