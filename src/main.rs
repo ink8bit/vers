d32aa7493bd8ee44a03fea4d50311c7365fa462b
ink8bit <ink8bit@users.noreply.github.com>
2020-12-05 20:56:03 +0300

    Add shell command

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use std::process::Command;
+use std::str;
+
+fn main() {
+    let output = Command::new("npm")
+        .arg("-v")
+        .output()
+        .expect("failed to execute command");
+
+    let status = output.status;
+    if !status.success() {
+        panic!("Error: can't execute `npm -v`");
+    }
+
+    let stdout = str::from_utf8(&output.stdout).unwrap();
+    dbg!(stdout);
+}
