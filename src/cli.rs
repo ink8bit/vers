37caa20bdd0a8a063a5ee9a1afad9d851920485c
ink8bit <ink8bit@users.noreply.github.com>
2020-12-05 22:19:04 +0300

    Add version command

---
+++ src/cli.rs
@@ -0,0 +1,31 @@
+use std::str::FromStr;
+
+use clap::{App, Arg, ArgMatches};
+
+pub enum Version {
+    Major,
+    Minor,
+    Patch,
+}
+
+impl FromStr for Version {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "major" => Ok(Version::Major),
+            "minor" => Ok(Version::Minor),
+            "patch" => Ok(Version::Patch),
+            _ => Err("no match"),
+        }
+    }
+}
+
+pub fn args() -> ArgMatches {
+    App::new("vers")
+        .version("0.1.0")
+        .about("vers is a CLI app which changes package version in JavaScript projects.")
+        .author("ink8bit")
+        .arg(Arg::from("<type> 'Version type'").possible_values(&["major", "minor", "patch"]))
+        .get_matches()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 use std::process::Command;
 use std::str;
 
+mod cli;
+use cli::Version;
+
 fn main() {
+    let args = cli::args();
+    // dbg!(args);
+    let mut ver_type = "";
+
+    let t = args.value_of_t("type").unwrap_or_else(|e| e.exit());
+    match t {
+        Version::Major => {
+            println!("major version");
+            ver_type = "major";
+        }
+        Version::Minor => println!("minor version"),
+        Version::Patch => println!("patch version"),
+    }
+
+    dbg!(ver_type);
+
     let output = Command::new("npm")
-        .arg("-v")
+        .arg("version")
+        .arg(ver_type)
         .output()
         .expect("failed to execute command");
 
     let status = output.status;
+    let stderr = str::from_utf8(&output.stderr).unwrap();
+
     if !status.success() {
-        panic!("Error: can't execute `npm -v`");
+        eprintln!("{}", stderr);
     }
 
     let stdout = str::from_utf8(&output.stdout).unwrap();
