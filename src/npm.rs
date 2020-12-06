fef0223d2b0f9dacbf5177836f81a3141b15aa78
ink8bit <ink8bit@users.noreply.github.com>
2020-12-06 13:06:24 +0300

    Add tests to version module

--- src/npm.rs
+++ src/npm.rs
@@ -11,11 +11,43 @@ pub fn version(ver_type: &str) -> Result<String, Box<dyn Error>> {
         .arg("--no-commit-hooks")
         .output()?;
 
-    let stdout = parse_std_out(out)?;
+    let stdout = parse_std_out(&out.stdout)?;
     Ok(stdout)
 }
 
-fn parse_std_out(out: std::process::Output) -> Result<String, Utf8Error> {
-    let std_out = str::from_utf8(&out.stdout)?;
+fn parse_std_out(out: &Vec<u8>) -> Result<String, Utf8Error> {
+    let std_out = str::from_utf8(&out)?;
     Ok(std_out.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_stdout_empty() {
+        let out: Vec<u8> = Vec::new();
+        assert_eq!(parse_std_out(&out).unwrap(), String::from(""));
+    }
+
+    #[test]
+    fn parse_stdout_valid_data() {
+        let out: Vec<u8> = Vec::from("v1.3.0");
+        assert_eq!(parse_std_out(&out).unwrap(), String::from("v1.3.0"));
+    }
+
+    #[test]
+    fn version_major() {
+        assert!(version("major").is_ok());
+    }
+
+    #[test]
+    fn version_minor() {
+        assert!(version("minor").is_ok());
+    }
+
+    #[test]
+    fn version_patch() {
+        assert!(version("patch").is_ok());
+    }
 }
