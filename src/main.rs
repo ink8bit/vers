abf4ed890e3c53b5f1c5d172fb54645dc0606212
ink8bit <ink8bit@users.noreply.github.com>
2020-12-06 11:28:50 +0300

    Fix version type handling

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,13 @@ use cli::Version;
 fn main() {
     let args = cli::args();
     // dbg!(args);
-    let mut ver_type = "";
 
     let t = args.value_of_t("type").unwrap_or_else(|e| e.exit());
-    match t {
-        Version::Major => {
-            println!("major version");
-            ver_type = "major";
-        }
-        Version::Minor => println!("minor version"),
-        Version::Patch => println!("patch version"),
-    }
+    let ver_type = match t {
+        Version::Major => "major",
+        Version::Minor => "minor",
+        Version::Patch => "patch",
+    };
 
     dbg!(ver_type);
 
