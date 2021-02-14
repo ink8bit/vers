c12820ad39b6a5f1223f5eb1c6bfcba18b64d601
ink8bit <ink8bit@users.noreply.github.com>
2021-02-14 22:50:57 +0300

    Remove version module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,28 @@
 mod changelog;
 mod git;
 mod npm;
-mod version;
 
 use changelog::{Changelog, Entry};
-use version::Version;
+use std::str::FromStr;
+
+pub enum Version {
+    Major,
+    Minor,
+    Patch,
+}
+
+impl FromStr for Version {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "major" => Ok(Version::Major),
+            "minor" => Ok(Version::Minor),
+            "patch" => Ok(Version::Patch),
+            _ => Err("no match"),
+        }
+    }
+}
 
 pub fn update(version: Version, info: &str, no_commit: bool) -> String {
     let ver_type = match version {
