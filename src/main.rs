ea70b0815db32efec467350799ef9c07a12e2abf
ink8bit <ink8bit@users.noreply.github.com>
2021-02-15 20:18:18 +0300

    Change version logic

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod cli;
 fn main() {
     let args = cli::args();
     let info = args.value_of("info").unwrap_or_default();
-    let version = args.value_of_t("type").unwrap_or_else(|e| e.exit());
+    let version = args.value_of("version").unwrap();
     let no_commit = args.is_present("no_commit");
 
     let v = vers::update(version, info, no_commit);
