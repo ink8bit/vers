cd391c551cbd33d750cf281cefa1f02719b80bb5
ink8bit <ink8bit@users.noreply.github.com>
2021-02-20 21:40:31 +0300

    Fix error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 mod cli;
 
-fn main() -> Result<(), vers::VersError> {
+fn main() {
     let args = cli::args();
     let info = args.value_of("info").unwrap_or_default();
     let version = args.value_of("version_type").unwrap();
     let no_commit = args.is_present("no_commit");
 
-    let v = vers::update(version, info, no_commit)?;
-
-    println!("Updated version: {}", v);
-
-    Ok(())
+    match vers::update(version, info, no_commit) {
+        Ok(v) => println!("Updated version: {}", v),
+        Err(e) => eprintln!("{}", e),
+    }
 }
