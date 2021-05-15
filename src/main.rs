d0569e54e6891b8bdbdb7222679dc1837c8287f0
ink8bit <ink8bit@users.noreply.github.com>
2021-05-15 20:02:28 +0300

    Add spinner to CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use colored::*;
+use terminal_spinners::{SpinnerBuilder, DOTS};
+
 mod cli;
 
 fn main() {
@@ -6,8 +9,23 @@ fn main() {
     let version = args.value_of("version_type").unwrap();
     let no_commit = args.is_present("no_commit");
 
+    let sp = SpinnerBuilder::new()
+        .spinner(&DOTS)
+        .text("Updating...")
+        .start();
+
     match vers::update(version, info, no_commit) {
-        Ok(v) => println!("Updated version: {}", v),
-        Err(e) => eprintln!("{}", e),
+        Ok(v) => {
+            let msg = format!("Successfully created version {}", v)
+                .green()
+                .to_string();
+            sp.text(msg);
+            sp.done();
+        }
+        Err(e) => {
+            let err_msg = format!("Error: {}", e).red().to_string();
+            sp.text(err_msg);
+            sp.done();
+        }
     }
 }
