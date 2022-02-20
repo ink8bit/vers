1d8392840deead5ad5587f4a14038ece5308ec05
ink8bit <ink8bit@users.noreply.github.com>
2022-02-20 18:01:39 +0300

    Fix cli config

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, ArgMatches};
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, ArgMatches, Command};
 
 pub(crate) fn args() -> ArgMatches {
-    App::new(crate_name!())
+    Command::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
         .author(crate_authors!())
@@ -15,14 +15,14 @@ pub(crate) fn args() -> ArgMatches {
                 .short('i')
                 .long("info")
                 .value_name("string")
-                .about("Sets info value")
+                .help("Sets info value")
                 .takes_value(true),
         )
         .arg(
             Arg::new("no_commit")
                 .short('n')
                 .long("no-commit")
-                .about("Prevents committing your changes"),
+                .help("Prevents committing your changes"),
         )
         .get_matches()
 }
