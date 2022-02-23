715c5c12e5393c95603e9c84b63b7c2ea8fc34a4
ink8bit <ink8bit@users.noreply.github.com>
2022-02-23 09:53:52 +0300

    Add docs

--- src/cli/version.rs
+++ src/cli/version.rs
@@ -2,6 +2,7 @@ use clap::Arg;
 
 pub(crate) const VERSION_ARG: &str = "version_type";
 
+/// Sets version type to update: major, minor, or patch.
 pub(crate) fn version() -> Arg<'static> {
     Arg::new(VERSION_ARG)
         .possible_values(&["major", "minor", "patch"])
