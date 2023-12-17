bed9cf7cbd330c9a8d0aba24ebf3c9cf7438a61c
ink8bit <ink8bit@users.noreply.github.com>
2023-12-17 23:54:03 +0300

    Update source to support clap v4.x

--- src/cli/info.rs
+++ src/cli/info.rs
@@ -1,13 +1,14 @@
-use clap::Arg;
+use clap::{Arg, ArgAction};
 
 pub(crate) const INFO_ARG: &str = "info";
 
 /// Adds content to `Changes` section in every changelog entry.
-pub(crate) fn info() -> Arg<'static> {
+pub(crate) fn info() -> Arg {
     Arg::new(INFO_ARG)
         .short('i')
         .long("info")
         .value_name("string")
         .help("Set info value")
-        .takes_value(true)
+        .num_args(1)
+        .action(ArgAction::Set)
 }
